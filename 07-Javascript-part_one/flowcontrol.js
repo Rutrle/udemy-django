0705831afbe906982b2c8af284541271e6addb66
Jakub <rutrle@seznam.cz>
2021-03-03 00:30:46 +0100

    more flow control

--- 07-Javascript-part_one/flowcontrol.js
+++ 07-Javascript-part_one/flowcontrol.js
@@ -11,3 +11,16 @@ if (temp > 80) {
     console.log("It is very cold out")
 }
 
+var ham = 10
+var cheese = 0
+var report = "blank"
+
+if (ham >= 10 && cheese >= 10) {
+    report = "strong sales of both ham and cheese!"
+} else if (ham === 0 && cheese === 0) {
+    report = "Nothing sold!"
+} else {
+    report = "we had some sales"
+}
+
+console.log(report);

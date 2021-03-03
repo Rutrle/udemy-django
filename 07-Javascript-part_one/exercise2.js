d930b13a9e7799f4197b0ebbdd4acf90548ed79b
Jakub <rutrle@seznam.cz>
2021-03-03 20:52:01 +0100

    control flow exercises

---
+++ 07-Javascript-part_one/exercise2.js
@@ -0,0 +1,24 @@
+var i = 0
+while (i < 5) {
+    console.log("Hello")
+    i++
+}
+
+
+for (var num = 0; num < 5; num++) {
+    console.log("Hello")
+}
+
+
+var j = 0
+while (j < 26) {
+    if (j % 2 === 1) {
+        console.log(j)
+    }
+    j = j + 1
+
+}
+
+for (var num = 1; num < 26; num = num + 2) {
+    console.log(num)
+}

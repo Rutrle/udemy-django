4913bce1f03c955bfebe3b2afbf2813b865a51d6
Jakub <rutrle@seznam.cz>
2021-03-08 23:42:28 +0100

    introduction to tuples, booleans and sets

---
+++ 12-python-level-one/tuples_sets_boolean.py
@@ -0,0 +1,23 @@
+t = (123, True, 'a')
+
+print(t)
+
+
+print(t)
+
+# Sets
+
+x = set()
+
+x.add(1)
+x.add(2)
+x.add(2)
+x.add(2)
+x.add(2)
+
+print(x)
+
+k = {1, 2, 5, 6, 4}
+
+print(k)
+print(type(k))

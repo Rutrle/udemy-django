fc8740486577cdee2c36efa34d9431bd2cf13a60
Jakub <rutrle@seznam.cz>
2021-03-17 00:52:36 +0100

    app creation exercise

--- 14-Django-level-one/ProTwo/AppTwo/views.py
+++ 14-Django-level-one/ProTwo/AppTwo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def indexview(request):
+    return HttpResponse('<em>My second app</em>')

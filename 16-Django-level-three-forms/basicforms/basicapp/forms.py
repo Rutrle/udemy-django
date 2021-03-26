627e7ed23cc7432fc0d837a371b394d8a799cb0f
Jakub <rutrle@seznam.cz>
2021-03-26 02:26:36 +0100

    botcatcher validation

--- 16-Django-level-three-forms/basicforms/basicapp/forms.py
+++ 16-Django-level-three-forms/basicforms/basicapp/forms.py
@@ -5,3 +5,9 @@ class FormName(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
+    botcatcher = forms.CharField(required=False, widget=forms.HiddenInput)
+
+    def clean_botcatcher(self):  # muse start with clean
+        botcatcher = self.cleaned_data['botcatcher']
+        if len(botcatcher) > 0:
+            raise forms.ValidationError("Gotcha bot!")

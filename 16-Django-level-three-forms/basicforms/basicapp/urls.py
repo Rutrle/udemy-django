7c3065a4c0323b6fa128c276e62a5924058c7f7d
Jakub <rutrle@seznam.cz>
2021-03-26 02:09:21 +0100

    basic forms

---
+++ 16-Django-level-three-forms/basicforms/basicapp/urls.py
@@ -0,0 +1,9 @@
+
+
+from django.urls import path
+from .views import index, form_name_view
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('formpage/', form_name_view, name='form_name'),
+]
--- 16-Django-level-three-forms/basicforms/basicapp/views.py
+++ 16-Django-level-three-forms/basicforms/basicapp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from .forms import FormName
 # Create your views here.
+
+
+def index(request):
+
+    return render(request, 'index.html')
+
+
+def form_name_view(request):
+    form = FormName()
+    if request.method == 'POST':
+        form = FormName(request.POST)
+        if form.is_valid():
+            print("Validation Success")
+            print(form.cleaned_data['name'])
+            print(form.cleaned_data['email'])
+            print(form.cleaned_data['text'])
+
+    return render(request, 'form_page.html', {'form': form})

b8c2ed93b6f2b5e9165da209a0d7979d2a562cad
Jakub <rutrle@seznam.cz>
2021-03-27 00:36:47 +0100

    upgraded form

--- 16-Django-level-three-forms/ProTwo/AppTwo/forms.py
+++ 16-Django-level-three-forms/ProTwo/AppTwo/forms.py
@@ -5,4 +5,4 @@ from .models import User
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ["first_name", "second_name"]
+        fields = "__all__"
--- 16-Django-level-three-forms/ProTwo/AppTwo/views.py
+++ 16-Django-level-three-forms/ProTwo/AppTwo/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import User
 from .forms import UserForm
 
@@ -20,5 +19,16 @@ def help_view(request):
 
 def input_view(request):
     form = UserForm()
+
+    if request.method == "POST":
+        form = UserForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return indexview(request)
+        else:
+            print("Error, form invalid")
+
     context = {"form": form}
+
     return render(request, "input_page.html", context=context)

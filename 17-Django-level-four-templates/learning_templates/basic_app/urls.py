3615548d7faead5d37d22cb1359908094ee49522
Jakub <rutrle@seznam.cz>
2021-03-27 02:04:10 +0100

    finished url names

---
+++ 17-Django-level-four-templates/learning_templates/basic_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import other, relative
+
+app_name = 'basic_app'
+urlpatterns = [
+    path('relative/', relative, name='relative'),
+    path('other/', other, name='other'),
+]

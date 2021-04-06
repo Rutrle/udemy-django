f6bcb94f1e833f526703875f6020134807f83fb8
Jakub <rutrle@seznam.cz>
2021-04-07 01:55:07 +0200

    created parts for authorizations

--- 21-Social-site-clone/simplesocial/simplesocial/settings.py
+++ 21-Social-site-clone/simplesocial/simplesocial/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+Template_dir = os.path.join(BASE_DIR, 'templates')
+# print(Template_dir)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -56,7 +59,7 @@ ROOT_URLCONF = 'simplesocial.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Template_dir],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
